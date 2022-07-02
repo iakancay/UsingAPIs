4c66a1c3302fbfcbbae1ea7e89c16ae7e0774fff
Ibrahim Akancay <iakancay@gmail.com>
2022-07-02 18:02:44 +0200

    promises completed

--- Week1/prep-exercises/1-catwalk-promises/index.js
+++ Week1/prep-exercises/1-catwalk-promises/index.js
@@ -5,9 +5,23 @@ const STEP_INTERVAL_MS = 50;
 const DANCE_TIME_MS = 5000;
 const DANCING_CAT_URL =
   'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+const WALKING_CAT_URL =
+  'http://www.anniemation.com/clip_art/images/cat-walk.gif';
 
 function walk(img, startPos, stopPos) {
-  return new Promise((resolve) => {
+  return new Promise(resolve => {
+    img.src = WALKING_CAT_URL;
+
+    let walkInterval = setInterval(() => {
+      img.style.left = startPos + 'px';
+      startPos += STEP_SIZE_PX;
+    }, STEP_INTERVAL_MS);
+    
+    setTimeout(() => {
+      clearInterval(walkInterval);
+      resolve(img);
+    }, ((stopPos - startPos) / STEP_SIZE_PX) * STEP_INTERVAL_MS);
+
     // Resolve this promise when the cat (`img`) has walked from `startPos` to
     // `stopPos`.
     // Make good use of the `STEP_INTERVAL_PX` and `STEP_INTERVAL_MS`
@@ -16,7 +30,12 @@ function walk(img, startPos, stopPos) {
 }
 
 function dance(img) {
-  return new Promise((resolve) => {
+  return new Promise(resolve => {
+    img.src = DANCING_CAT_URL;
+    setTimeout(() => {
+      resolve(img);
+    }, DANCE_TIME_MS);
+
     // Switch the `.src` of the `img` from the walking cat to the dancing cat
     // and, after a timeout, reset the `img` back to the walking cat. Then
     // resolve the promise.
@@ -29,6 +48,18 @@ function catWalk() {
   const startPos = -img.width;
   const centerPos = (window.innerWidth - img.width) / 2;
   const stopPos = window.innerWidth;
+
+  walk(img, startPos, centerPos)
+    .then(resolvedValue => dance(resolvedValue))
+    .then(resolvedValue => walk(resolvedValue, centerPos, stopPos))
+    .then(resolvedValue => walk(resolvedValue, startPos, centerPos))
+    .then(resolvedValue => dance(resolvedValue))
+    .then(resolvedValue => walk(resolvedValue, centerPos, stopPos))
+    .then(resolvedValue => walk(resolvedValue, startPos, centerPos))
+    .then(resolvedValue => dance(resolvedValue))
+    .then(resolvedValue => walk(resolvedValue, centerPos, stopPos))
+    .then(resolvedValue => walk(resolvedValue, startPos, centerPos))
+
 
   // Use the `walk()` and `dance()` functions to let the cat do the following:
   // 1. Walk from `startPos` to `centerPos`.
